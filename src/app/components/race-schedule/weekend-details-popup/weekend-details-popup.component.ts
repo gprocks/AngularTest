a4908ae9edd27d827f84cfd765141b17b1019c7c
gprocks <36969049+gprocks@users.noreply.github.com>
2021-11-19 17:27:03 +0000

    Remove bad semi-colon

--- src/app/components/race-schedule/weekend-details-popup/weekend-details-popup.component.ts
+++ src/app/components/race-schedule/weekend-details-popup/weekend-details-popup.component.ts
@@ -30,7 +30,7 @@ export class WeekendDetailsPopupComponent implements OnInit {
       .pipe(
         map(result => this.getBackgroundImage(result[0].flags.png)),
         catchError(()=>{
-          return "background: #3f51b5;"
+          return "background: #3f51b5"
         })
         );
 
